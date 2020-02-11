9321c8c1508f207797a11ed2d6661d42df472bc8
Sujin <sujin.v2px@gmail.com>
2020-02-11 11:58:16 +0545

    shortest path implementation

--- src/core/solution.py
+++ src/core/solution.py
@@ -1,8 +1,22 @@
 import json
+
+import src.core.utils as utils
 from src.core.vrpmanager import VRPManager, Strategy, Attribute
 
 DSE = "dse"
 OUTLETS = "outlets"
+
+route_index = [0]
+
+
+def _find_next_outlet(distance_graph, previous_index):
+    min_distance = 9999.9
+    next_index = -1
+    for index, distance in enumerate(distance_graph[previous_index]):
+        if min_distance > distance:
+            next_index = index
+    route_index.append(next_index)
+    return next_index
 
 
 def _handle_cheap_path_strategy(vrp_manager, data):
@@ -18,7 +32,9 @@ def _handle_cheap_path_strategy(vrp_manager, data):
         while route_distance < vrp_manager.max_distance or len(outcome[OUTLETS]) < vrp_manager.max_outlets:
             outcome[OUTLETS].append(json.loads(data[Attribute.OUTLETS][index].toJSON()))
             previous_index = index
-
+            next_index = _find_next_outlet(data[Attribute.DISTANCE_MATRIX], previous_index)
+            outcome[OUTLETS].append(json.loads(data[Attribute.OUTLETS][next_index].toJSON()))
+            route_distance += utils.distance_callback(data[Attribute.DISTANCE_MATRIX], previous_index, next_index)
 
     return results
 
